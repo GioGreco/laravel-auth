7afbf7a3abd74185c93b03aae233f97715bfb6c3
GioGreco <grecogiorgio.97@gmail.com>
2023-01-10 17:16:05 +0100

    back-office: display fake projects titles

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h1>Index</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li><a class="btn btn-primary text-white btn-sm" href="{{route('admin.projects.show', $project->slug)}}" title="View Project">{{$project->title}}</a></li>
+
+        @endforeach
+    </ul>
 @endsection
